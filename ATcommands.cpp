b1748ad288d5e9e8e6f8bf562ff729f39dad0b1d
Timothy Woo <botletics@gmail.com>
2017-10-20 16:51:53 -0400

    Add files via upload

---
+++ ATcommands.cpp
@@ -0,0 +1,81 @@
+#include "Arduino.h"
+#include "ATcommands.h"
+
+ATcommands::ATcommands(int8_t rst, bool lineEnding) {
+  pinMode(rst, OUTPUT);
+
+  _rst = rst;
+  mySerial = 0;
+  newline = lineEnding;
+}
+
+ATcommands::begin(Stream &port) {
+  mySerial = &port;
+}
+
+void ATcommands::reset(uint8_t pulse, uint16_t duration) {
+  if (pulse == LOW) {
+    digitalWrite(_rst, HIGH);
+    delay(10);
+    digitalWrite(_rst, LOW);
+    delay(duration);
+    digitalWrite(_rst, HIGH);
+  }
+  else if (pulse == HIGH) {
+    digitalWrite(_rst, LOW);
+    delay(10);
+    digitalWrite(_rst, HIGH);
+    delay(duration);
+    digitalWrite(_rst, LOW);
+  }
+}
+
+bool ATcommands::sendBlindCommand(char *command) {
+  Serial.print(F("\t---> ")); Serial.println(command);
+  if (newline) mySerial->println(command);
+  else mySerial->print(command);
+}
+
+bool ATcommands::sendCommand(char *command, uint16_t timeout) {
+  while(mySerial->available()) mySerial->read(); // Clear input buffer
+
+  Serial.print(F("\t---> ")); Serial.println(command);
+  if (newline) mySerial->println(command);
+  else mySerial->print(command);
+
+  bool gotReply = false;
+  timer = millis();
+
+  while (!gotReply && millis() - timer < timeout) {
+  	if (mySerial->available()) gotReply = true;
+  }
+
+  if (gotReply) return true;
+  else return false;
+}
+
+bool ATcommands::sendCommand(char *command, char *reply, uint16_t timeout) {
+  while(mySerial->available()) mySerial->read(); // Clear input buffer
+
+  Serial.print(F("\t---> ")); Serial.println(command);
+  if (newline) mySerial->println(command);
+  else mySerial->print(command);
+
+  uint16_t idx = 0;
+  bool replyMatch = false;
+  timer = millis();
+
+  while (!replyMatch && millis() - timer < timeout) {
+  	if (mySerial->available()) {
+  	  replybuffer[idx] = mySerial->read();
+  	  idx++;
+  	  // if (strstr(replybuffer, reply) != NULL) replyMatch = true; // Only checks if desired reply is inside replybuffer
+  	  if (strcmp(replybuffer, reply) == 0) replyMatch = true; // This means the reply must start with the desired reply to be successful
+  	}
+  }
+
+  Serial.print(F("\t<--- ")); Serial.println(replybuffer);
+
+  if (replyMatch) return true;
+  else return false;
+}
---
+++ ATcommands.h
@@ -0,0 +1,27 @@
+#ifndef ATcommands_h
+#define ATcommands_h
+
+#include "Arduino.h"
+
+#define default_pulse_ms 100
+#define default_timeout_ms 500
+
+
+class ATcommands {
+  public:
+    ATcommands(int8_t rst, bool newline = false);
+    begin(Stream &port);
+    void reset(uint8_t pulse, uint16_t duration = default_pulse_ms);
+    bool sendBlindCommand(char *command);
+    bool sendCommand(char *command, uint16_t timeout = default_timeout_ms);
+    bool sendCommand(char *command, char *reply, uint16_t timeout = default_timeout_ms);
+  
+  private:
+    int8_t _rst;
+    bool newline = false;
+    char replybuffer[255];
+    unsigned long timer;
+    Stream *mySerial;
+};
+
+#endif
